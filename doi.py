361054b85b40a89861b2a528eb8b6d00041dd752
root <root@instance-1.c.supple-serenity-116311.internal>
2018-06-09 12:27:39 +0000

    ar

--- doi.py
+++ doi.py
@@ -6,7 +6,7 @@ str(my_etiquette)
 works = Works()
 works = Works(etiquette=my_etiquette)
 def fetch_issns():
-    with open('doien_an.tsv') as f:
+    with open('doien_as.tsv') as f:
         lines = f.readlines()[1:]  # skip line 1 (table headers)
 
         articles = []
@@ -40,7 +40,7 @@ def fetch_results():
 
 
 def write_to_file(results):
-    with open('doien_an_ext.tsv', 'w') as f:
+    with open('doien_as_ext.tsv', 'w') as f:
         for result in results:
             for index, item in enumerate(result):
                 if index < (len(result) - 1):
